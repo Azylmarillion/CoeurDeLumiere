4bf05238bc00e05c88b7510053a7e7ca0e782adc
Lucas Guibert <lucasjoestar@outlook.fr>
2019-10-08 16:35:42 +0200

    Start

---
+++ Assets/Scripts/Lucas/PAF_Fruit.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PAF_Fruit : MonoBehaviour
+{
+    #region Fields / Properties
+
+    #endregion
+
+    #region Methods
+
+    #region Original Methods
+
+    #endregion
+
+    #region Unity Methods
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    #endregion
+
+    #endregion
+}

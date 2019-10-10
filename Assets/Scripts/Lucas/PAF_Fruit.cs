79cca68fd4b6ae15dd87a14e96fdd26c9c72eb90
Lucas Guibert <lucasjoestar@outlook.fr>
2019-10-10 18:10:28 +0200

    Physique Fruit, jour 2

--- Assets/Scripts/Lucas/PAF_Fruit.cs
+++ Assets/Scripts/Lucas/PAF_Fruit.cs
@@ -7,23 +7,28 @@ using UnityEngine;
 public class PAF_Fruit : MonoBehaviour
 {
     #region Events
-    private event Action OnOverlap = null;
+
     #endregion
 
     #region Fields / Properties
 
     #region Static & Constants
     /// <summary>
-    /// All fruits 
+    /// Reference list of all fruits currently on the arena.
     /// </summary>
-    public static PAF_Fruit[] fruits = new PAF_Fruit[] { };
+    private static List<PAF_Fruit> arenaFruits = new List<PAF_Fruit>();
+
+    /// <summary>
+    /// All fruits currently on the arena !
+    /// </summary>
+    public static PAF_Fruit[] ArenaFruits { get { return arenaFruits.ToArray(); } }
     #endregion
 
     #region Parameters
     /// <summary>
     /// Collider of the object.
     /// </summary>
-    [SerializeField] private new Collider collider = null;
+    [SerializeField] private new SphereCollider collider = null;
 
     /// <summary>
     /// Weight of the fruit, influencing its movement force.
@@ -83,13 +88,27 @@ public class PAF_Fruit : MonoBehaviour
     /// <returns></returns>
     private IEnumerator ApplyForce()
     {
-        Vector3 _newPos;
+        Vector3 _normal = new Vector3(velocity.z, velocity.y, -velocity.x);
+        Vector3 _raycastPos;
+        RaycastHit _hit;
 
         while (velocity != Vector3.zero)
         {
-            _newPos = transform.position + velocity;
+            // Take closest
+            if (RaycastVelocityMovement(_raycastPos = collider.bounds.center, out _hit) ||
+                RaycastVelocityMovement(_raycastPos += _normal * collider.radius, out _hit) ||
+                RaycastVelocityMovement(_raycastPos -= _normal * collider.radius * 2, out _hit))
+            {
+                transform.position = transform.position + (velocity * _hit.distance);
 
+                // Set velocity after collision
+                velocity = new Vector3(Mathf.Abs(velocity.x) * Mathf.Sign(_hit.normal.x) * .5f, Mathf.Abs(velocity.y) * Mathf.Sign(_hit.normal.y) * .5f, Mathf.Abs(velocity.z) * Mathf.Sign(_hit.normal.z) * .5f);
 
+                // Push the touched fruit if one
+                PAF_Fruit _fruit = _hit.collider.GetComponent<PAF_Fruit>();
+                
+                //if (_fruit) _fruit.AddForce(???);
+            }
 
             yield return new WaitForFixedUpdate();
         }
@@ -97,13 +116,9 @@ public class PAF_Fruit : MonoBehaviour
         applyForceCoroutine = null;
     }
 
-    /// <summary>
-    /// Method called when starting to collide with another collider.
-    /// </summary>
-    /// <param name="_other"></param>
-    private void OnColliderEnter(Collider _collider)
+    private bool RaycastVelocityMovement(Vector3 _raycastPos, out RaycastHit _hit)
     {
-
+        return Physics.Raycast(_raycastPos, velocity, out _hit, (collider.bounds.center + velocity).magnitude);
     }
     #endregion
 
@@ -111,12 +126,23 @@ public class PAF_Fruit : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        #if UNITY_EDITOR
         if (!collider)
         {
-            collider = GetComponent<Collider>();
+            collider = GetComponent<SphereCollider>();
             if (!collider) Debug.LogError($"The Fruit \"{name}\" don't have collider attached to it !");
+            else Debug.LogWarning($"The Fruit \"{name}\" don't have collider linked to it !");
         }
+        #endif
 
+        // Add this fruit to the arena list on start !
+        arenaFruits.Add(this);
+    }
+
+    private void OnDestroy()
+    {
+        // Remove this fruit from the arena list when destroyed
+        if (arenaFruits.Contains(this)) arenaFruits.Remove(this);
     }
     #endregion
 

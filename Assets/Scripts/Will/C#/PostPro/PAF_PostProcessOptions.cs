ccc3ec78cf8df037fc58fa85cbf5ea32ff4f22d0
Wiwi la malice <william.comminges@gmail.com>
2019-10-16 10:15:46 +0200

    PostProcessing
     PostPro plugin +prefabs folder + cameraPostPro prefab+PAF_PostProcessOption

---
+++ Assets/Scripts/Will/C#/PostPro/PAF_PostProcessOptions.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PAF_PostProcessOptions : MonoBehaviour
+{
+    #region Events
+
+    #endregion
+
+    #region Fields / Properties
+
+    #endregion
+
+    #region Methods
+    #region Original Methods   
+
+    #endregion
+
+    #region Unity Methods
+    void Awake()
+    {
+
+    }
+    void Start()
+    {
+
+    }
+    #endregion
+    #endregion
+}

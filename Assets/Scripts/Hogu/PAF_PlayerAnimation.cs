861cd7c979f0fdb7831221a41db0e3a4669fea29
Alexis <thiebautalexis01@gmail.com>
2019-11-08 18:46:15 +0100

    Commit Modif Player Animation + Particles Hit

--- Assets/Scripts/Hogu/PAF_PlayerAnimation.cs
+++ Assets/Scripts/Hogu/PAF_PlayerAnimation.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public class PAF_PlayerAnimation : MonoBehaviour
 {
+    [SerializeField] private ParticleSystem m_system = null; 
+
+
     public void StepSounds()
     {
         AudioSource _audio = GetComponentInParent<AudioSource>();
         if (_audio) _audio.PlayOneShot(PAF_GameManager.Instance?.SoundDatas.GetStepsPlayer());
+    }
+
+    public void CastTrail()
+    {
+        if (m_system == null) return;
+        m_system.Play(); 
     }
 }

a3b71e2d8b9c5dd2e35b7f156f28d230acb25fd1
H.G <hugo.guerquin@outlook.fr>
2019-11-13 11:19:18 +0100

    Sol dalles

--- Assets/Scripts/Hogu/PAF_DalleManager.cs
+++ Assets/Scripts/Hogu/PAF_DalleManager.cs
@@ -8,8 +8,8 @@ public class PAF_DalleManager : MonoBehaviour
 {
     public static PAF_DalleManager I { get; private set; }
 
-    [SerializeField] List<PAF_Dalle> allDallesToFall = new List<PAF_Dalle>();
     [SerializeField] List<PAF_Dalle> allRespawnableDalles = new List<PAF_Dalle>();
+    [SerializeField] List<GameObject> allDallesGroups = new List<GameObject>();
     public List<PAF_Dalle> AllRespawnableDalles { get { return allRespawnableDalles; } }
     [SerializeField] Transform center = null;
     [SerializeField] float fallDelay = .25f;
@@ -19,7 +19,7 @@ public class PAF_DalleManager : MonoBehaviour
     public Material[] DalleMaterials { get { return dalleMaterials; } }
     public float Shift { get { return shift; } }
 
-    public bool IsReady => center && allDallesToFall.Count > 0;
+    public bool IsReady => center && allDallesGroups.Count > 0;
 
     [SerializeField, Range(0.0f, 1.0f)] private float m_screenshakeDuration = .1f;
     [SerializeField, Range(0.0f, 1.0f)] private float m_screenshakeForce = .1f;
@@ -38,9 +38,9 @@ public class PAF_DalleManager : MonoBehaviour
 
     void InitDalle()
     {
-        foreach (PAF_Dalle _dalle in allDallesToFall)
+        foreach (GameObject _go in allDallesGroups)
         {
-            _dalle.gameObject.AddComponent<AudioSource>();
+            _go.AddComponent<AudioSource>();
         }
     }
 
@@ -48,7 +48,7 @@ public class PAF_DalleManager : MonoBehaviour
     {
         if (!IsReady) return;
         //allDallesToFall = allDallesToFall.OrderByDescending(d => Vector3.Distance(d.transform.position, center.position)).ToList(); // EXTERIEUR VERS INTERIEUR
-        allDallesToFall = allDallesToFall.OrderByDescending(i => Guid.NewGuid()).ToList(); // RANDOM
+        allDallesGroups = allDallesGroups.OrderByDescending(i => Guid.NewGuid()).ToList(); // RANDOM
         StartCoroutine(DelayFall());
     }
     
@@ -58,7 +58,7 @@ public class PAF_DalleManager : MonoBehaviour
         float _timer = 0;
         Vector2 _offset = Vector2.zero;
 
-        for (int i = 0; i < allDallesToFall.Count; i++)
+        for (int i = 0; i < allDallesGroups.Count; i++)
         {
             while (_timer < m_screenshakeDuration)
             {
@@ -69,7 +69,30 @@ public class PAF_DalleManager : MonoBehaviour
             }
             _timer = 0;
             Camera.main.transform.position = _initialPosition;
-            allDallesToFall[i].Fall();
+            List<PAF_Dalle> _allDallesToFall = allDallesGroups[i].GetComponentsInChildren<PAF_Dalle>().ToList();
+
+            _allDallesToFall = _allDallesToFall.OrderByDescending(d => Vector3.Distance(d.transform.position, center.position)).ToList(); // EXTERIEUR VERS INTERIEUR
+            //_allDallesToFall = _allDallesToFall.OrderByDescending(d => Guid.NewGuid()).ToList(); // RANDOM
+
+            AudioClip _clip = PAF_GameManager.Instance?.SoundDatas.GetDalleFalling();
+            AudioSource _source = allDallesGroups[i].GetComponent<AudioSource>();
+            if (_source && _clip) _source.PlayOneShot(_clip, .7f);
+
+            // CHUTE AU FUR ET A MESURE
+            for (int j = 0; j < _allDallesToFall.Count; j++)
+            {
+                _allDallesToFall[j].Fall();
+                yield return new WaitForSeconds(.05f);
+            }
+
+            //// CHUTE D'UN COUP
+            //foreach (PAF_Dalle _dalle in _allDallesToFall)
+            //{
+            //    _dalle.Fall();
+            //}
+
+
+            //allDallesToFall[i].Fall();
             yield return new WaitForSeconds(fallDelay);
         }
     }
